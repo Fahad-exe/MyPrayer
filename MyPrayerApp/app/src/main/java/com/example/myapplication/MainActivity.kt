ef870eb0fc42ceb433a3a0b7d946e58451fff8d5
Fahad-exe <72553922+Fahad-exe@users.noreply.github.com>
2023-04-20 05:57:10 +0100

    QuranFragment finished

--- MyPrayerApp/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyPrayerApp/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myapplication.fragments.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val homeFragment = HomeFragment()
+        val prayerTimeFragment = PrayerTimeFragment()
+        val qiblaFragment = QiblaFragment()
+        val quranFragment = QuranFragment()
+        val settingsFragment = SettingsFragment()
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        makeCurrentFragment(homeFragment)
+
+
+
+        bottom_navigation.setOnItemSelectedListener() {
+            when(it.itemId){
+                R.id.Home_ic-> makeCurrentFragment(homeFragment)
+                R.id.Time_ic -> makeCurrentFragment(prayerTimeFragment)
+                R.id.Qibla_ic -> makeCurrentFragment(qiblaFragment)
+                R.id.Quran_ic -> makeCurrentFragment(quranFragment)
+                R.id.Settings_ic-> makeCurrentFragment(settingsFragment)
+            }
+            true
+        }
+
+
+
     }
 
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 }
