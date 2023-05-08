1d66c375930ab6f24c49c951f1b982658c76d914
Fahad-exe <72553922+Fahad-exe@users.noreply.github.com>
2023-05-08 01:51:14 +0100

    Finished Quran And HAlfway through Qibla

--- MyPrayerApp/app/src/main/java/com/example/myapplication/QiblaActivity.kt
+++ MyPrayerApp/app/src/main/java/com/example/myapplication/QiblaActivity.kt
@@ -1,11 +1,79 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.compose.runtime.invalidateGroupsWithKey
+import androidx.core.content.ContextCompat.getSystemService
+
+private lateinit var mSensorManager: SensorManager
+private lateinit var mMagnetometer: Sensor
+private lateinit var mAccelerometer: Sensor
+private lateinit var mImageViewCompass: AppCompatImageView
+private var mGravityValues = FloatArray(3)
+private val mAccelerationValues = FloatArray(3)
+private val mRotationMatrix = FloatArray(9)
+private var mLastDirectionInDegrees = 0f
+
 
 class QiblaActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qibla)
+        mImageViewCompass = findViewById(R.id.imgViewCompass)
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    }
+    override fun onResume(){
+        super.onResume()
+        mSensorManager.registerListener(mSensorListener,mMagnetometer,SensorManager.SENSOR_DELAY_FASTEST)
+        mSensorManager.registerListener(mSensorListener,mAccelerometer,SensorManager.SENSOR_DELAY_FASTEST)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSensorManager.unregisterListener(mSensorListener)
     }
 }
+
+private val mSensorListener = object : SensorEventListener {
+
+
+    override fun onSensorChanged(event: SensorEvent) {
+        calculateCompassDirection(event)
+    }
+
+    private fun calculateCompassDirection(event: SensorEvent) {
+
+        when (event.sensor.type){
+            Sensor.TYPE_MAGNETIC_FIELD -> mGravityValues = event.values.clone()}
+        val success = SensorManager.getRotationMatrix(mRotationMatrix,null,mAccelerationValues,mGravityValues)
+        if (success){
+            val orientationValues = FloatArray(3)
+            SensorManager.getOrientation(mRotationMatrix,orientationValues)
+            val azimuth = Math.toDegrees(-orientationValues[0].toDouble()).toFloat()
+            val rotateAnimation = RotateAnimation(mLastDirectionInDegrees,azimuth,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f)
+            rotateAnimation.duration = 50
+            rotateAnimation.fillAfter = true
+            mImageViewCompass!!.startAnimation(rotateAnimation)
+            mLastDirectionInDegrees = azimuth
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        //Nothing to do
+    }
+
+
+}
+
--- MyPrayerApp/app/src/main/java/com/example/myapplication/QuranActivity.kt
+++ MyPrayerApp/app/src/main/java/com/example/myapplication/QuranActivity.kt
@@ -4,6 +4,7 @@ package com.example.myapplication
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
@@ -12,10 +13,6 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_quran.*
-import java.io.ByteArrayOutputStream
-import java.io.IOException
-import java.io.InputStream
-
 
 class QuranActivity : AppCompatActivity() {
 
@@ -24,17 +21,19 @@ class QuranActivity : AppCompatActivity() {
     var playPause: FloatingActionButton? = null
     var mMediaPlayer: MediaPlayer? = null
     var mseekBar: SeekBar? = null
-    var surahList = mutableListOf(R.raw.salah_bukhatir_al_fatihah)
-    var currentsurahIndex : Int = 0
-    var surahName:TextView?  = null
+    var surahList = mutableListOf(R.raw.salah_bukhatir_al_fatihah, R.raw.salah_bukhatir_al_kawthar)
+    var currentsurahIndex: Int = 0
+    var surahName: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quran)
         mseekBar = findViewById(R.id.seekbar)
         playPause = findViewById(R.id.play_button)
-        controlsound(surahList[0])
+        controlsound(surahList[currentsurahIndex])
         surahName = findViewById(R.id.SurahTitle)
+        mMediaPlayer = MediaPlayer.create(this, surahList[currentsurahIndex])
+
 
     }
 
@@ -45,24 +44,42 @@ class QuranActivity : AppCompatActivity() {
 
             if (isPlaying) {
                 pause()
-            }else{
-                mMediaPlayer = MediaPlayer.create(this,id)
-                Log.d("QuranActivity", "ID: ${mMediaPlayer!!.audioSessionId}")
 
-                initialiseSeekBar()
+            } else {
+                MediaPlayer.create(this, surahList[currentsurahIndex])
+                MediaPlayer.MEDIA_INFO_METADATA_UPDATE
+                Log.d("QuranActivity", "ID: ${mMediaPlayer!!.audioSessionId}")
                 play()
+
+
             }
             isPlaying = !isPlaying
-     }
-        next_button.setOnClickListener{
+        }
+        next_button.setOnClickListener {
+            if (!isPlaying) {
+
+                mMediaPlayer?.pause()
+                mMediaPlayer?.release()
+
+
+
+            }else{
+
+                    playNext()
+
+            }
 
         }
-        previous_button.setOnClickListener{
-
+        previous_button.setOnClickListener {
+            if (isPlaying) {
+                mMediaPlayer?.release()
+                playPrev()
+            }
+            playPrev()
         }
         mseekBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(mseekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if (fromUser){
+                if (fromUser) {
                     mMediaPlayer?.seekTo(progress)
                 }
             }
@@ -84,49 +101,57 @@ class QuranActivity : AppCompatActivity() {
             override fun run() {
                 try {
                     mseekBar?.progress = mMediaPlayer!!.currentPosition
-                    handler.postDelayed(this,1000)
-                }catch (e:Exception){
+                    handler.postDelayed(this, 1000)
+                } catch (e: Exception) {
                     mseekBar?.progress = 0
                 }
             }
-        },0)
+        }, 0)
     }
 
     fun play() {
-         mMediaPlayer?.start()
-        Log.d("QuranActivity", "Duration: ${mMediaPlayer!!.duration/1000} seconds")
+        mMediaPlayer?.start()
+        initialiseSeekBar()
+        Log.d("QuranActivity", "Duration: ${mMediaPlayer!!.duration / 1000} seconds")
 
-        val pauseImage = ContextCompat.getDrawable(this,android.R.drawable.ic_media_pause)
-         playPause?.setImageDrawable(pauseImage)
+        val pauseImage = ContextCompat.getDrawable(this, android.R.drawable.ic_media_pause)
+        playPause?.setImageDrawable(pauseImage)
 
 
     }
 
-     fun pause() {
+    fun pause() {
+
         mMediaPlayer?.pause()
-         Log.d("QuranActivity", "Paused at: ${mMediaPlayer!!.currentPosition/1000} seconds")
+        Log.d("QuranActivity", "Paused at: ${mMediaPlayer!!.currentPosition / 1000} seconds")
 
-         val startImage = ContextCompat.getDrawable(this,android.R.drawable.ic_media_play)
-         playPause?.setImageDrawable(startImage)
+        val startImage = ContextCompat.getDrawable(this, android.R.drawable.ic_media_play)
+        playPause?.setImageDrawable(startImage)
     }
 
-    fun playNext(){
+    fun playNext() {
 
-        if (currentsurahIndex < (surahList.size - 1)){
-            controlsound(currentsurahIndex + 1)
+        if (currentsurahIndex < (surahList.size - 1)) {
             currentsurahIndex += 1
-        }else{
-            controlsound(surahList[0])
+            mMediaPlayer
+            MediaPlayer.create(this, surahList[currentsurahIndex])
+            mMediaPlayer?.start()
+
+        } else {
+            currentsurahIndex = 0
         }
     }
 
-    fun playPrev(){
+    fun playPrev() {
 
-        if (currentsurahIndex < (surahList.size + 1)){
+        if (currentsurahIndex < (surahList.size + 1)) {
             controlsound(currentsurahIndex - 1)
             currentsurahIndex -= 1
-        }else{
-            controlsound(surahList[0])
+            MediaPlayer.create(this, surahList[currentsurahIndex])
+            mMediaPlayer?.start()
+
+        } else {
+            currentsurahIndex = 0
         }
     }
 
@@ -135,9 +160,11 @@ class QuranActivity : AppCompatActivity() {
         clearMediaPlayer()
     }
 
-    private fun clearMediaPlayer(){
+    private fun clearMediaPlayer() {
         mMediaPlayer!!.stop()
         mMediaPlayer!!.release()
     }
 
+
 }
+
