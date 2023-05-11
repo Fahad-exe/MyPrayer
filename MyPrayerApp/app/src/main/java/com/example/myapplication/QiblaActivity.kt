875dcfed292dc036b2df6bfa46b5783b98258a33
Fahad-exe <72553922+Fahad-exe@users.noreply.github.com>
2023-05-11 08:46:48 +0100

    bug-fixes

--- MyPrayerApp/app/src/main/java/com/example/myapplication/QiblaActivity.kt
+++ MyPrayerApp/app/src/main/java/com/example/myapplication/QiblaActivity.kt
@@ -1,5 +1,4 @@
 package com.example.myapplication
-
 import android.content.Context
 import android.content.Intent
 import android.hardware.Sensor
@@ -11,13 +10,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
-import android.widget.ImageView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.compose.runtime.invalidateGroupsWithKey
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.graphics.rotationMatrix
 import com.google.android.material.bottomnavigation.BottomNavigationView
-
 private lateinit var mSensorManager: SensorManager
 private lateinit var mMagnetometer: Sensor
 private lateinit var mAccelerometer: Sensor
@@ -31,16 +28,11 @@ private val orientation = FloatArray(3)
 private var kabaahLatitude = 25.4106386
 private  var kabaahLongitude =  -54.189238
 private  var mAzimuth = 0f
-
-
 class QiblaActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qibla)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-
         bottomNavigationView.setOnItemSelectedListener() { menuItem ->
             when (menuItem.itemId) {
                 R.id.home_ic -> {
@@ -61,82 +53,65 @@ class QiblaActivity : AppCompatActivity() {
                 else -> false
             }
         }
-
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-
         mImageViewCompass = findViewById(R.id.imgViewCompass)
-
         mLocation = Location(" ")
         mLocation.latitude = kabaahLatitude
         mLocation.longitude = kabaahLongitude
     }
-    override fun onResume(){
+
+    override fun onResume() {
         super.onResume()
         mSensorManager.registerListener(mSensorListener,mMagnetometer,SensorManager.SENSOR_DELAY_FASTEST)
         mSensorManager.registerListener(mSensorListener,mAccelerometer,SensorManager.SENSOR_DELAY_FASTEST)
     }
-
     override fun onPause() {
         super.onPause()
         mSensorManager.unregisterListener(mSensorListener)
     }
 }
-
-private val mSensorListener = object : SensorEventListener {
-
-
-    override fun onSensorChanged(event: SensorEvent) {
-
-        if (event.sensor == null){
-            return
+       private val mSensorListener = object : SensorEventListener{
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor == null) {
+                return
+            }
+            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                System.arraycopy(event.values, 0, mAccelerationValues, 0, mAccelerationValues.size)
+            } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+                System.arraycopy(event.values, 0, mGravityValues, 0, mGravityValues.size)
+            }
+            val success = SensorManager.getRotationMatrix(
+                mRotationMatrix,
+                null,
+                mAccelerationValues,
+                mGravityValues
+            )
+            if (success) {
+                SensorManager.getOrientation(mRotationMatrix, orientation)
+                val azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat()
+                val azimuthFix = (azimuth + 360) % 360
+                val distance = mLocation.distanceTo(mLocation)
+                val bearing = mLocation.bearingTo(mLocation)
+                val direction = (azimuthFix + bearing) % 360
+                val rotateAnimation = RotateAnimation(
+                    mAzimuth,
+                    -direction,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f
+                )
+                rotateAnimation.duration = 1000
+                rotateAnimation.fillAfter = true
+                mImageViewCompass.startAnimation(rotateAnimation)
+                mAzimuth = -direction
+            }
         }
-        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER){
-            System.arraycopy(event.values,0, mAccelerationValues, 0, mAccelerationValues.size)
-        }else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD){
-            System.arraycopy(event.values,0, mGravityValues,0, mGravityValues.size)
-        }
-        val success = SensorManager.getRotationMatrix(mRotationMatrix,null, mAccelerationValues,mGravityValues)
-        if (success){
-            SensorManager.getOrientation(mRotationMatrix,orientation)
-            val azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat()
-            val azimuthFix = (azimuth + 360) % 360
-
-            val distance = mLocation.distanceTo(mLocation)
-            val bearing = mLocation.bearingTo(mLocation)
-
-            val direction =(azimuthFix + bearing) % 360
-
-            val rotateAnimation = RotateAnimation(mAzimuth,-direction,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f)
-            rotateAnimation.duration = 1000
-            rotateAnimation.fillAfter = true
-            mImageViewCompass.startAnimation(rotateAnimation)
-            mAzimuth = -direction
-        }
+           override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+               //Nothing to do
+           }
     }
 
-    /*private fun calculateCompassDirection(event: SensorEvent) {
-
-        when (event.sensor.type){
-            Sensor.TYPE_MAGNETIC_FIELD -> mGravityValues = event.values.clone()}
-        val success = SensorManager.getRotationMatrix(mRotationMatrix,null,mAccelerationValues,mGravityValues)
-        if (success){
-            val orientationValues = FloatArray(3)
-            SensorManager.getOrientation(mRotationMatrix,orientationValues)
-            val azimuth = Math.toDegrees(-orientationValues[0].toDouble()).toFloat()
-            val rotateAnimation = RotateAnimation(mLastDirectionInDegrees,azimuth,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f)
-            rotateAnimation.duration = 50
-            rotateAnimation.fillAfter = true
-            mImageViewCompass!!.startAnimation(rotateAnimation)
-            mLastDirectionInDegrees = azimuth
-        }
-    }
-*/
-    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
-        //Nothing to do
-    }
-
-
-}
 
