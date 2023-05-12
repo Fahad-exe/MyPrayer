c4c092558d0b462c0b1ce9947c2193f84ce4255c
Fahad-exe <72553922+Fahad-exe@users.noreply.github.com>
2023-05-12 18:16:16 +0100

    bug fixes

--- MyPrayerApp/app/src/main/java/com/example/myapplication/QiblaActivity.kt
+++ MyPrayerApp/app/src/main/java/com/example/myapplication/QiblaActivity.kt
@@ -1,34 +1,41 @@
 package com.example.myapplication
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
 import androidx.appcompat.widget.AppCompatImageView
-import androidx.compose.runtime.invalidateGroupsWithKey
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.graphics.rotationMatrix
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.security.Permission
+
+
 private lateinit var mSensorManager: SensorManager
 private lateinit var mMagnetometer: Sensor
 private lateinit var mAccelerometer: Sensor
 private lateinit var mLocation: Location
 private lateinit var mImageViewCompass: AppCompatImageView
-private lateinit var mImageArrow: AppCompatImageView
 private var mGravityValues = FloatArray(3)
-private val mAccelerationValues = FloatArray(3)
-private val mRotationMatrix = FloatArray(9)
-private val orientation = FloatArray(3)
-private var kabaahLatitude = 25.4106386
-private  var kabaahLongitude =  -54.189238
+private var mGeomagneticValues = FloatArray(3)
+private var kabaahLatitude = 21.422487
+private  var kabaahLongitude =  39.826206
 private  var mAzimuth = 0f
-class QiblaActivity : AppCompatActivity() {
+
+private val PERMISSION_REQUEST_CODE = 1001
+
+class QiblaActivity : AppCompatActivity(), SensorEventListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qibla)
@@ -60,58 +67,80 @@ class QiblaActivity : AppCompatActivity() {
         mLocation = Location(" ")
         mLocation.latitude = kabaahLatitude
         mLocation.longitude = kabaahLongitude
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION ,Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_REQUEST_CODE)
+            } else {
+                getLocation()
+            }
+        } else {
+            getLocation()
+        }
     }
-
     override fun onResume() {
         super.onResume()
-        mSensorManager.registerListener(mSensorListener,mMagnetometer,SensorManager.SENSOR_DELAY_FASTEST)
-        mSensorManager.registerListener(mSensorListener,mAccelerometer,SensorManager.SENSOR_DELAY_FASTEST)
+        mSensorManager.registerListener(this,mMagnetometer,SensorManager.SENSOR_DELAY_GAME)
+        mSensorManager.registerListener(this,mAccelerometer,SensorManager.SENSOR_DELAY_GAME)
     }
     override fun onPause() {
         super.onPause()
-        mSensorManager.unregisterListener(mSensorListener)
+        mSensorManager.unregisterListener(this)
     }
-}
-       private val mSensorListener = object : SensorEventListener{
-        override fun onSensorChanged(event: SensorEvent) {
-            if (event.sensor == null) {
-                return
-            }
-            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-                System.arraycopy(event.values, 0, mAccelerationValues, 0, mAccelerationValues.size)
-            } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
-                System.arraycopy(event.values, 0, mGravityValues, 0, mGravityValues.size)
-            }
-            val success = SensorManager.getRotationMatrix(
-                mRotationMatrix,
-                null,
-                mAccelerationValues,
-                mGravityValues
-            )
-            if (success) {
-                SensorManager.getOrientation(mRotationMatrix, orientation)
-                val azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat()
-                val azimuthFix = (azimuth + 360) % 360
-                val distance = mLocation.distanceTo(mLocation)
-                val bearing = mLocation.bearingTo(mLocation)
-                val direction = (azimuthFix + bearing) % 360
-                val rotateAnimation = RotateAnimation(
-                    mAzimuth,
-                    -direction,
-                    Animation.RELATIVE_TO_SELF,
-                    0.5f,
-                    Animation.RELATIVE_TO_SELF,
-                    0.5f
-                )
-                rotateAnimation.duration = 1000
-                rotateAnimation.fillAfter = true
-                mImageViewCompass.startAnimation(rotateAnimation)
-                mAzimuth = -direction
+    override fun onSensorChanged(event: SensorEvent?) {
+      if (event?.sensor == mMagnetometer){
+          mGeomagneticValues = event.values
+      }
+        if (event?.sensor == mAccelerometer){
+            mGravityValues = event.values
+        }
+        val R= FloatArray(9)
+        val I =FloatArray(9)
+        val success = SensorManager.getRotationMatrix(R,I, mGravityValues, mGeomagneticValues)
+        if (success){
+            val orientation = FloatArray(3)
+            SensorManager.getOrientation(R,orientation)
+            val azimuth = Math.toDegrees(orientation[0].toDouble()).toFloat()
+            val rotateAnimation = RotateAnimation(mAzimuth, -azimuth,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f)
+            rotateAnimation.duration = 400
+            rotateAnimation.fillAfter = true
+            mImageViewCompass.startAnimation(rotateAnimation)
+            mAzimuth = -azimuth
+        }
+    }
+
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+
+    private fun getLocation() {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            val mLocation: Location? = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            if (mLocation !=null){
+                 kabaahLatitude = mLocation.latitude
+                 kabaahLongitude = mLocation.longitude
+
+
             }
         }
-           override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
-               //Nothing to do
-           }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] ==PackageManager.PERMISSION_GRANTED){getLocation()}
+        }
+    }
+}
+
+
+
+
 
 
